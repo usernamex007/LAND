c3251ff9bf43f9f622c29b0468ff2caf19c3e954
usernamex007 <sonalixsachin@gmail.com>
2025-02-09 00:18:43 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,7 +16,7 @@ logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(levelname)s - %(
 # 🛠 Configuration
 API_ID = 23120489
 API_HASH = "ccfc629708e2f8a05c31ebe7961b5f92"
-BOT_TOKEN = "7984449177:AAFq5h_10P6yLlqv5CsjB_WJ8dRLK7U_JIw"
+BOT_TOKEN = "7984449177:AAEjwcdh-OlUhJ5E_L26jSZfmCPFDuNK7d4"
 
 # 📂 SQLite Database setup
 conn = sqlite3.connect('bot_config.db')
